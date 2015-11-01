953de6f1d7124d542395935100f1d4916a42b5bc
Eric Andre <eric@meetidesign.com>
2015-10-31 22:44:00 -0500

    add XOR and XNOR logical operators, cus why not

--- utils.js
+++ utils.js
@@ -3,5 +3,7 @@ var typecheck = require('./lib/typecheck');
 utils.is = typecheck.is;
 utils.type = typecheck.type;
 utils.wrapURL = require('./lib/wrapURL');
+utils.xor = require('./lib/xor');
+utils.xnor = require('./lib/xnor.js');
 
 module.exports = utils;
